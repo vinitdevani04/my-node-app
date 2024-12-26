3312003eb9a6a5bbba3123f289551dea7dd99bec
Vinit <159558408+vntsolution@users.noreply.github.com>
2024-12-26 15:44:52 +0530

    Remove unnecessary newline at the end of the auth routes file

--- routes/auth_routs.js
+++ routes/auth_routs.js
@@ -7,5 +7,4 @@ authRoutes.post("/login", authController.login);
 authRoutes.get("/users", authController.getAllUser);
 router.delete('/delete', authController.deleteUser);
 
-
 module.exports = authRoutes;
