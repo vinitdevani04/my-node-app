09a55e90235a9f531a476472a619c1377271a026
Vinit <159558408+vntsolution@users.noreply.github.com>
2024-12-26 16:44:23 +0530

    Add CORS middleware to enable cross-origin requests

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors'); // Import cors
 const app = express();
 require('dotenv').config();
 const port = 3000;
 
+app.use(cors()); // Enable CORS
 app.use(express.json());
 console.log("MONGODB_URI:>>>>>>>>>", process.env.MONGODB_URI);
 
