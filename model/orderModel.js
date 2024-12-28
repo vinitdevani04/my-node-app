281703a87710747f36942ffaf4f34c6aaffde4ae
Vinit <159558408+vntsolution@users.noreply.github.com>
2024-12-28 15:51:04 +0530

    Refactor order model: uncomment required fields for validation

--- model/orderModel.js
+++ model/orderModel.js
@@ -3,40 +3,40 @@ const mongoose = require('mongoose');
 const orderSchema = new mongoose.Schema({
     phone: {
         type: String,
-        // required: true
+        required: true
     },
     email: {
         type: String,
-        // required: true
+        required: true
     },
     productName: {
         type: String,
-        // required: true
+        required: true
     },
     image: {
         type: String,
-        // required: true
+        required: true
     },
     color: {
         type: String,
-        // required: true
+        required: true
     },
     price: {
         type: Number,
-        // required: true
+        required: true
     },
     storage: {
         type: String,
-        // required: true
+        required: true
     },
     quantity: {
         type: Number,
-        // required: true
+        required: true
     },
     paymentType: {
         type: String,
         enum: ['BTC', 'SOLANA', 'ETH', 'USDT'],
-        // required: true
+        required: true
     },
     orderStatus: {
         type: String,
@@ -48,24 +48,24 @@ const orderSchema = new mongoose.Schema({
     },
     transactionHash: {
         type: String,
-        // required: true,
+        required: true,
         unique: true
     },
     walletId: {
         type: String,
-        // required: true
+        required: true
     },
     address: {
         type: String,
-        // required: true
+        required: true
     },
     city: {
         type: String,
-        // required: true
+        required: true
     },
     pincode: {
         type: String,
-        // required: true
+        required: true
     },
     orderDateTime: {
         type: Date,
