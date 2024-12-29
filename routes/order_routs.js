f347661b42d3b5c5ad0a770820f8aabd590a1fa9
Vinit <159558408+vntsolution@users.noreply.github.com>
2024-12-29 13:15:43 +0530

    Change PATCH requests to PUT for order status and verified payment updates

--- routes/order_routs.js
+++ routes/order_routs.js
@@ -6,8 +6,8 @@ orderRoutes.post("/create", orderController.createOrder);
 orderRoutes.get("/orders", orderController.getAllOrders);
 orderRoutes.get("/:id", orderController.getOrderById);
 orderRoutes.delete("/:id", orderController.deleteOrder);
-orderRoutes.patch("/:id/oderstatus", orderController.changeOrderStatus);
-orderRoutes.patch("/:id/verifiedPayment", orderController.updateVerifiedPaymentStatus);
+orderRoutes.put("/:id/oderstatus", orderController.changeOrderStatus);
+orderRoutes.put("/:id/verifiedPayment", orderController.updateVerifiedPaymentStatus);
 orderRoutes.post("/search", orderController.getOrdersByEmail);
 
 module.exports = orderRoutes;
